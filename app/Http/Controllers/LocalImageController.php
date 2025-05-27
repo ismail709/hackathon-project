26b3966b96f44a262e59631256600e85e5d2bc97
ismail709 <ismailrhilal@gmail.com>
2025-05-27 12:02:24 +0100

    add images to locales

---
+++ app/Http/Controllers/LocalImageController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LocalImageController extends Controller
+{
+    //
+}
---
+++ database/migrations/2025_05_27_110130_create_local_images_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Local;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('local_images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Local::class)->constrained()->cascadeOnDelete();
+            $table->string('image_path');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('local_images');
+    }
+};
